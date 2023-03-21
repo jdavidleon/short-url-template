5c956b82ea901dde5897523c0d96c108747fbe9e
jdavidleon <86429650+jdavidleon@users.noreply.github.com>
2023-03-21 09:31:07 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const connectDB = require('./db');
+const routes = require('./routes/routes');
+
+
+/*   BASIC CONGIFURARION   */
+const port = process.env.PORT || 5000;
+
+// CONNECT
+connectDB();
+
+
+/*   MIDDLEWARE   */
+app.use(cors());
+app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/*   ROUTES   */ 
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+//
+app.use('/api', routes);
+
+
+/*   LISTENER   */
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
